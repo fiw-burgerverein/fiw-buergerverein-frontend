04683af804a46cb3545adf5ced6feacfd204afa3
vistah <37378402+vistah@users.noreply.github.com>
2019-12-10 18:47:30 +0100

    Update formula.component.ts

--- formula.component.ts
+++ formula.component.ts
@@ -12,7 +12,13 @@ export class FormulaComponent implements OnInit {
 
   ngOnInit() {
   }
+  
+  email = new FormControl('', [Validators.required, Validators.email]);
 
-
+  getErrorMessage() {
+    return this.email.hasError('required') ? 'Sie müssen eine gültige E-Mail-Adresse eingeben' :
+      this.email.hasError('email') ? 'Keine gültige E-Mail-Adresse' :
+        '';
+  }
 }
 
