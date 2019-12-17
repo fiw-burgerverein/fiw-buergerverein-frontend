cdc070294d9abafeda820957ccfaa26b89a7aa42
Kuebra <Kuebra@kbozers-MacBook-Pro.local>
2019-12-17 19:23:30 +0100

    routing eingebaut für Antrag und Registrierung

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {FormulaComponent} from "./formula/formula.component";
+import {ContentComponent} from "./content/content.component";
+import {RegistrierungComponent} from "./registrierung/registrierung.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'formula', component: FormulaComponent},
+  {path:'', component: ContentComponent},
+  {path:'registrieren', component:RegistrierungComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
