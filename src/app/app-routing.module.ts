c0f2fecb9acde9535fc2b437b376cb9595484737
Kuebra <Kuebra@kbozers-MacBook-Pro.local>
2019-12-18 17:08:07 +0100

    formulare miteinander verbunden

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {FormulaComponent} from "./formula/formula.component";
 import {ContentComponent} from "./content/content.component";
 import {RegistrierungComponent} from "./registrierung/registrierung.component";
+import {FormulaProjektComponent} from "./formula-projekt/formula-projekt.component";
 
 
 const routes: Routes = [
   {path:'formula', component: FormulaComponent},
   {path:'', component: ContentComponent},
-  {path:'registrieren', component:RegistrierungComponent}
+  {path:'registrieren', component:RegistrierungComponent},
+  {path:'formula-projekt', component:FormulaProjektComponent}
 
 ];
 
